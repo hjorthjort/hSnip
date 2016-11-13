dd1d0dd67a8dd9114ce3c61fecb989b104cffdd7
hjorthjort <rikard.hjort@gmail.com>
2016-11-13 22:47:40 +0100

    isSet and getFlagArg in Flags

--- flags.hs
+++ flags.hs
@@ -1,7 +1,13 @@
-module Flags(getFlags) where
+module Flags(
+    getFlags,
+    isSet,
+    getFlagArg
+            )
+    where
 
 type Flag = String
 type FlagArg = Maybe String
+type FlagList = [(Flag, FlagArg)]
 
 getFlags :: [String] -> [(Flag, FlagArg)]
 getFlags = map (\flag -> (parseFlag flag, if getFlagArg flag == []
@@ -15,3 +21,12 @@ getFlags = map (\flag -> (parseFlag flag, if getFlagArg flag == []
         isFlag _ = False
         parseFlag = takeWhile (/='=') . dropWhile (=='-')
         getFlagArg = drop 1 . dropWhile (/='=')
+
+isSet :: Flag -> FlagList -> Bool
+isSet f fs
+  | lookup f fs == Nothing = False
+  | otherwise = True
+
+getFlagArg :: Flag -> FlagList -> FlagArg
+getFlagArg f fs | isSet f fs = (\(Just a) -> a) $ lookup f fs
+                | otherwise = Nothing
