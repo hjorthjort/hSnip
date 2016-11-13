8eaeb75de4120fc37bfdcb7f1cc194be863bbf1a
hjorthjort <rikard.hjort@gmail.com>
2016-11-13 21:12:14 +0100

    Command line version

--- snip.hs
+++ snip.hs
@@ -1,12 +1,36 @@
+-- | A snippet manager.
+-- | Inspired by Learn You Some Haskell todo program.
 module Snippet where
 
-import System.IO (withFile, hPutStr, IOMode(AppendMode))
+import Data.List
+import System.Directory
+import System.Environment
+import System.IO
 
 defaultFileName = "snip.txt"
 defaultDirName = "."
+defaultPath = defaultDirName ++ "/" ++ defaultFileName
+
+-- Association list that connects command line options with functions.
+dispatch :: [(String, [String] -> IO ())]
+dispatch = [
+    ("view", view)
+  , ("add", add)
+  , ("remove", remove)
+           ]
+
+view [fileName] = do
+    allSnips <- readFile fileName
+    putStr allSnips
+
+add (x:xs) = undefined
+remove (x:xs) = undefined
 
 main = do
-    newTodo <- getLine
-    withFile (defaultDirName ++ "/" ++ defaultFileName) AppendMode
-        (`hPutStr` (newTodo ++ "\n"))
+    (command:args) <- getArgs
+    let (Just action) = lookup command dispatch
+    action args
+    -- newTodo <- getLine
+    -- withFile defaultPath AppendMode
+    --     (`hPutStr` (newTodo ++ "\n"))
 
